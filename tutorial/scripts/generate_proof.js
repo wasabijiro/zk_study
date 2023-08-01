a4f544054375747840d4c3147ca23119a0d75b97
wasabijiro <shushu011023@gmail.com>
2023-08-01 12:03:38 +0900

    [tutorial] create proof and verify success

---
+++ tutorial/scripts/generate_proof.js
@@ -0,0 +1,27 @@
+const snarkjs = require("snarkjs");
+const fs = require('fs');
+
+/**
+ * main script
+ */
+const main = async() => {
+  // generate proof data
+  const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+    { in: 10 },
+    "build/sample_js/sample.wasm",
+    "zkey/circuit_0000.zkey");
+
+  console.log("public:", publicSignals);
+  console.log("proof:", proof);
+
+  const publicSignalsJson = JSON.stringify(publicSignals);
+  const proofJson = JSON.stringify(proof);
+
+  // jsonファイルでアウトプット
+  fs.writeFileSync('proof/public.json', publicSignalsJson);
+  fs.writeFileSync('proof/proof.json', proofJson);
+
+  process.exit(0);
+};
+
+main();
